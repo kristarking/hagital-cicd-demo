5acfc64051447a6b9a375d8e6553a70252f095ce
Chris <ojedayochristopher@gmail.com>
2026-09-15 19:15:25 +0100

    Port 80 added to the main.tf file

--- main.tf
+++ main.tf
@@ -69,6 +69,17 @@ resource "azurerm_network_security_group" "hagital-nsg" {
     destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
 }
 }
 #NIC
